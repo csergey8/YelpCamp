19ee19698c28231e9efbe394ba17ea8a505d8cf9
csergey8 <c.sergey@protonmail.com>
2017-09-30 16:47:11 +0300

    add delete camp route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -87,6 +87,17 @@ router.put('/:id', function (req, res) {
         })
 });
 
+// DELETE CAMPGROUD ROUTE
+
+router.delete('/:id', function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect('/capgrounds/' + req.params.id);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
+});
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
